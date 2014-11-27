9e2cd8bb1ee2734f63555d0adad57ce9610b291a
Kevin Shin <kevinshin92@gmail.com>
2014-11-26 19:05:01 -0800

    Changed push state to work

--- push_state.py
+++ push_state.py
@@ -11,20 +11,20 @@ def upload_dir(args):
     replica_config = args.cfg
     assert(os.path.exists(dir_to_push))
     assert(os.path.isdir(dir_to_push))
-    with open(replica_config) as ips:
-        for line in ips.readlines():
-            ip_addr = line.strip()
-            command = 'rsync -r --exclude \".*/\" ' + dir_to_push + \
-                      ' pi@' + ip_addr + ':~/cs244b/'
-            print command
-            os.system(command)            
+    ips_f = open(replica_config, 'r')
+    for line in ips_f:
+        ip_addr = line.strip()
+        command = 'rsync -r --exclude \".*/\" ' + dir_to_push + \
+                 ' pi@' + ip_addr + ':~/cs244b/'
+        print "Command Is: " + command
+        os.system(command)
+    ips_f.close()
 
 # python remote_pi_control -f ../nsync
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Raspberry Pi Remote Control")
     parser.add_argument("-f", type=str, default=None)
     parser.add_argument("-cfg", type=str, default=REPLICA_IP_FILE)
-
     args = parser.parse_args()
     if args.f != None:
         upload_dir(args)
