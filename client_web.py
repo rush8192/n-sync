7c9a8987b947a97a274a2ae0359847e320ebba54
Jeff Han <jeffninghan@gmail.com>
2014-12-09 15:48:21 -0800

    front end interface implemented

--- client_web.py
+++ client_web.py
@@ -1,3 +1,4 @@
+from flask import Flask, render_template
 import sys
 import urllib2
 import argparse
@@ -7,48 +8,76 @@ import os
 from constants import *
 import utils
 import time
+from flask_bootstrap import Bootstrap
 
 MASTER_IP = "192.168.1.138"
 PORT = "8000"
 
+app = Flask(__name__)
+Bootstrap(app)
+
+def get_song_files():
+    song_files = []
+    all_files = os.listdir(MUSIC_DIR)
+    for f in all_files:
+        if f.count('.mp3') != 0:
+            song_files.append(f)
+    return song_files
+
 def get_url(command):
     return "http://" + MASTER_IP + ":" + PORT + "/" + command
 
+@app.route("/")
+def index():
+    global songfiles, playlist
+    return render_template('index.html', song_files = song_files, playlist = playlist)
+
+@app.route("/play")
 def play():
     url = get_url(PLAY)
     try:
         r = urllib2.urlopen(url)
-        print utils.unserialize_response(r.read())
+        resp = utils.unserialize_response(r.read())
     except Exception:
-        print "Error in Playing Song"
+        resp = {"success": False, "msg": "Error in Playing Song"}
+    return resp
 
+@app.route("/pause")
+def pause():
+    url = get_url(PAUSE)
+    try:
+        r = urllib2.urlopen(url)
+        resp = utils.unserialize_response(r.read())
+    except Exception:
+        resp = {"success": False, "msg": "Error in Pausing Song"}
+    return resp
+
+@app.route("/forward")
 def forward():
+    global playlist
     url = get_url(FORWARD)
     try:
         r = urllib2.urlopen(url)
-        print utils.unserialize_response(r.read())
+        resp = utils.unserialize_response(r.read())
+        if len(playlist) != 0:
+            playlist.pop(0)
     except Exception:
-        print "Error in Forwarding Song"
+        resp = {"success": False, "msg": "Error in Forwarding Song"}
+    return resp
 
+@app.route("/backward")
 def backward():
     url = get_url(BACKWARD)
     try:
         r = urllib2.urlopen(url)
-        print utils.unserialize_response(r.read())
+        resp = utils.unserialize_response(r.read())
     except Exception:
-        print "Error in Backwarding Song"
+        resp = {"success": False, "msg": "Error in Backwarding Song"}
+    return resp
 
-def pause():
-    url = get_url(PAUSE)
-    print url
-    try:
-        r = urllib2.urlopen(url)
-        print utils.unserialize_response(r.read())
-    except Exception:
-        print "Error in Pausing Song"
-
-# Send song hash first, then full song if needed
-def load_song(song_path):
+@app.route("/load/<song_file>")
+def load(song_file):
+    song_path = MUSIC_DIR + song_file
     m = hashlib.md5()
     assert(os.path.exists(song_path))
     with open(song_path, 'r') as f:
@@ -76,8 +105,14 @@ def load_song(song_path):
         if not master_response['success']:
             print master_response['msg']
         print master_response['client_req_id']
+    return 'load'
 
-def enqueue_song(song_path):
+@app.route("/enqueue/<song_file>")
+def enqueue(song_file):
+    global playlist
+    playlist.append(song_file)
+    return 'songthing'
+    song_path = MUSIC_DIR + song_file
     load_song(song_path)
     assert(os.path.exists(song_path))
     with open(song_path, 'r') as f:
@@ -92,46 +127,15 @@ def enqueue_song(song_path):
         master_response = utils.unserialize_response(r.read())
         if master_response['success'] == True:
             print song_path + ' has been enqueued'
+            playlist.append(song_file)
         else:
             print song_path + ' cannot be enqueued'
         print master_response['client_req_id']
+    return 'enqueue'
 
 if __name__ == "__main__":
-    if (len(sys.argv) == 1):
-        print "Usage:"
-        print "python ./client_stub.py -[pfbu]"
-        print "-p play, -f forward, -b backward, -u pause"
-        sys.exit()
-
-    parser = argparse.ArgumentParser(description='Client Stub Nsync.')
-    parser.add_argument('-pl', action='store_true', help='play first song')
-    parser.add_argument('-pa', action='store_true', help='pause at master offset')
-    parser.add_argument('-f', action='store_true', help='move to next song')
-    parser.add_argument('-b', action='store_true', help='move back in queue')
-    parser.add_argument('-l', type=str, default=None)
-    parser.add_argument('-q', type=str, default=None)
-    args = parser.parse_args()
-    start = time.time()
-    op = None
-    if args.pl:
-        play()
-        op = PLAY
-    if args.pa:
-        pause()
-        op = PAUSE
-    if args.f:
-        forward()
-        op = FORWARD
-    if args.b:
-        backward()
-        op = BACKWARD
-    if args.l != None:
-        load_song(args.l)
-        op = LOAD
-    if args.q != None:
-        enqueue_song(args.q)
-        op = ENQUEUE
-    print op + ' took ' + str(time.time() - start) + ' sec'
-    
-
-
+    song_files = get_song_files()
+    loaded_songs = []
+    playlist = []
+    current_song = None
+    app.run()
